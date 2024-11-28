5d4c0164161357411466e57cd19b2fb1fe14d01e
Matteo Cipolla <mati_teo@live.it>
2024-11-28 16:07:44 +0100

    feat:add init project

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1 Chiediamo all'utente il numero di km di dovrà percorrere.
+
+
+// Chiediamo l'età del passeggero.
+
+//Calcoliamo il prezzo del biglietto senza sconto in base ai km 
+
+//Calcoliamo il prezzo del biglietto con il 20% se è un minorenne
+
+//Calcoliamo il prezzo del biglietto con il 40% se è un over 65
+
+//Prezzo finale del biglietto con due decimali
+
